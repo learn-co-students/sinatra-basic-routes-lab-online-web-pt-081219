8efd96b2a30c2a010bf4376009e6e27ea7d9e26c
saramccombs <theSaraMcCombs@gmail.com>
2019-11-09 09:26:28 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+    end
+
+    get '/name' do
+        "My name is Sara."
+    end
+
+    get '/hometown' do
+        "My hometown is Fairmont, WV."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Rx'"
+    end
+
 end

5307a64c19875ac23f870505a81ffe17f9786b89
Phyllis Kan <Phyllis Kan>
2019-11-10 14:56:23 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Shosho"
+    end
+
+    get '/hometown' do
+        "My hometown is Tokyo"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Walk On Water"
+    end
 end

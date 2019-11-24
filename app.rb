347555843db4a7013eb93fdde3537f0f843b87db
Baktiar Miah <baktiar.m@gmail.com>
2019-11-24 22:35:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    'My name is Baktiar Miah '
+  end
+
+   get '/hometown' do
+     'My hometown is Brooklyn, NY'
+   end
+
+   get '/favorite-song' do
+     'My favorite song is Numb - Linkin Park '
+   end
+
 end
